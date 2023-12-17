6f3add26f15335139842dee15314d8b87d93b286
ngocptblaplafla <phanthanhngocblaplafla@gmail.com>
2023-12-17 20:23:57 +0700

    remove fail-if-warnings

--- sample/example.sh
+++ sample/example.sh
@@ -2,5 +2,5 @@ docker run --rm -v $(pwd)/sample:/workspace pandoc-texlive-full:latest \
       --pdf-engine=xelatex --listings --lua-filter include-files.lua --lua-filter tables-rules.lua  \
       --filter pandoc-crossref --filter pandoc-latex-environment --lua-filter column-div.lua  \
       --lua-filter include-code-files.lua --lua-filter multibib.lua  --lua-filter diagram.lua \
-      --fail-if-warnings -H example.tex example.md -s -o result.pdf
+      -H example.tex example.md -s -o result.pdf
 
